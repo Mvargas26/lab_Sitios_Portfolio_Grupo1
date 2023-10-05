2d06b4745038a5494d83a50a87bc17a515767ab8
Michael Vargas S <michaelvargas47@hotmail.com>
2023-10-04 22:26:33 -0600

    Se agrega categoria faltante

--- App/Db.php
+++ App/Db.php
@@ -10,7 +10,7 @@ class Db {
         try {
             $this->conexion = new PDO("mysql:host={$this->host};dbname={$this->dbnombre}", $this->usuario, $this->contrasena);
             $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo 'db conectada';
+            //echo 'db conectada';
             return $this->conexion;
         } catch (PDOException $e) {
             die("Error en la conexión a la base de datos: " . $e->getMessage());
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once('App/Db.php');
 include_once('App/Producto.php');
 
-$db = new Db(); 
+$db = new Db();
 $producto = new Producto($db);
 
 $productos = $producto->mostrarProductos();
@@ -10,26 +10,59 @@ $productos = $producto->mostrarProductos();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/css/style.css" rel="stylesheet" />
     <title>Document</title>
 </head>
+
 <body>
-<?php if ($productos) : ?>
-        <ul>
+    <div class="main">
+
+        <h1>LAB SITIOS PORTFOLIO GRUPO 1</h1>
+        <hr>
+
+        <h2>PORTFOLIO</h2>
+
+        <div id="myBtnContainer">
+            <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+            <button class="btn" onclick="filterSelection('nature')"> Nature</button>
+            <button class="btn" onclick="filterSelection('cars')"> Cars</button>
+            <button class="btn" onclick="filterSelection('people')"> People</button>
+            <button class="btn" onclick="filterSelection('dogs')"> Dogs</button>
+        </div>
+
+        <div class="row">
+
+            
+
+
+            <?php if ($productos) : ?>
             <?php foreach ($productos as $producto) : ?>
-                <li>
-                    Nombre: <?php echo $producto['Nombre']; ?><br>
-                    Precio: <?php echo $producto['Imagen']; ?><br>
-                    Descripción: <?php echo $producto['Categoria']; ?><br>
-                    <!-- Agrega aquí más detalles de los productos según tu base de datos -->
-                    <hr>
-                </li>
+                <div class="column <?php echo $producto['Categoria'];?>">
+                <div class="content">
+                    <img src="assets/images/<?php echo $producto['Imagen']; ?>" alt="Lights" style="width:100%">
+                    <h4> <?php echo $producto['Nombre']; ?></h4>
+                    <p> <?php echo $producto['Descripcion']; ?></p>
+                </div>
+            </div>
             <?php endforeach; ?>
-        </ul>
     <?php else : ?>
         <p>No se encontraron productos.</p>
     <?php endif; ?>
+
+        </div>
+
+
+
+
+
+
+        <!-- END MAIN -->
+    </div>
+    <script src="./assets/js/scripts.js"></script>
 </body>
+
 </html>
